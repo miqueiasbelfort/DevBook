7f2dce7d9977d636a82576654fe69e1e50163d9a
miqueiasbelfort <kawasousa1554@gmail.com>
2023-01-13 08:58:19 -0300

    database connection

--- API/src/controllers/usuarios.go
+++ API/src/controllers/usuarios.go
@@ -1,9 +1,39 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func CriarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criar um usuario"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario models.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuarioID)))
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
---
+++ API/src/repositorios/usuarios.go
@@ -0,0 +1,38 @@
+package repositorios
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type usuarios struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
+	return &usuarios{db}
+}
+
+// Criar insere um usuário no banco de dados
+func (repositorio usuarios) Criar(usuario models.Usuario) (uint64, error) {
+	statment, erro := repositorio.db.Prepare(
+		"insert into usuarios (nome, nick, email, senha) values (?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statment.Close()
+
+	resultado, erro := statment.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+
+}
